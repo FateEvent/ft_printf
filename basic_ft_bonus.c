d47f332f188e68e08a8e6d54ef3509072542ff2b
FateEvent <fabio.venturini.1989@gmail.com>
2022-03-09 17:01:19 +0100

    j'essaie

---
+++ basic_ft_bonus.c
@@ -0,0 +1,114 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   basic_ft.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: faventur <faventur@student.42mulhouse.fr>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/07 14:56:34 by faventur          #+#    #+#             */
+/*   Updated: 2022/03/07 16:46:16 by faventur         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	ft_putaddr_count(unsigned long long nbr, int *n_char)
+{
+	int	i;
+	int	ret[100];
+
+	i = 0;
+	if (nbr == 0)
+	{
+		ft_putstr_count("0x0", n_char);
+		return (*n_char);
+	}
+	while (nbr)
+	{
+		ret[i] = nbr % 16;
+		nbr /= 16;
+		i++;
+	}
+	ft_putstr_count("0x", n_char);
+	while (--i >= 0)
+		ft_putchar_count("0123456789abcdef"[ret[i]], n_char);
+	return (*n_char);
+}
+
+int	ft_putnbr_count(int nbr, char *base, int *n_char)
+{
+	int			i;
+	int			ret[100];
+	long int	x;
+
+	i = 0;
+	x = nbr;
+	if (nbr == 0)
+		ft_putchar_count(0 + '0', n_char);
+	if (x < 0)
+	{
+		x *= -1;
+		ft_putchar_count('-', n_char);
+	}
+	while (x > 0)
+	{
+		ret[i] = x % 10;
+		x /= 10;
+		i++;
+	}
+	while (--i >= 0)
+		ft_putchar_count(base[ret[i]], n_char);
+	return (*n_char);
+}
+
+int	ft_puthexa(unsigned int nbr, char *base, int *n_char)
+{
+	int			i;
+	int			ret[100];
+
+	i = 0;
+	if (nbr == 0)
+		ft_putchar_count(0 + '0', n_char);
+	if (nbr < 0)
+	{
+		nbr *= -1;
+		ft_putchar_count('-', n_char);
+	}
+	while (nbr > 0)
+	{
+		ret[i] = nbr % 16;
+		nbr /= 16;
+		i++;
+	}
+	while (--i >= 0)
+		ft_putchar_count(base[ret[i]], n_char);
+	return (*n_char);
+}
+
+int	ft_put_unsigned(unsigned int nbr, char *base, int *n_char)
+{
+	int			size_base;
+	int			i;
+	int			ret[100];
+	long int	x;
+
+	size_base = 0;
+	i = 0;
+	x = nbr;
+	if (nbr == 0)
+		ft_putchar_count(0 + '0', n_char);
+	if (x < 0)
+	{
+		x *= -1;
+		ft_putchar_count('-', n_char);
+	}
+	while (x > 0)
+	{
+		ret[i] = x % size_base;
+		x /= size_base;
+		i++;
+	}
+	while (--i >= 0)
+		ft_putchar_count(base[ret[i]], n_char);
+	return (*n_char);
+}
