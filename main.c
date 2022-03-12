f656dd88c48af3d202f32069cd4ff3ffa37d4e43
FateEvent <fabio.venturini.1989@gmail.com>
2022-03-12 11:37:02 +0100

    floating point exception

--- main.c
+++ main.c
@@ -10,9 +10,11 @@ int main()
 	int tab[1];
 	tab[0] = 0;
 //	ft_printf("%d\n%i\n%d\n%i\n%d\n%i\n%d\n", 1, 5, 8, 9, 15, 90, -2147483648);
-	ft_printf("%o\n%o\n%o\n%o\n%o\n%o\n", 1, 5, 8, 9, 15, 90);
-	printf("%o\n%o\n%o\n%o\n%o\n%o\n", 1, 5, 8, 9, 15, 90);
-	ft_printf("%d\n", ft_printf("my ptf: %d\n%p\n%x\n%o\n", -2147483648, &x, -187, 15));
-	printf("%d\n", printf("real ptf: %d\n%p\n%x\n%o\n", -2147483648, &x, -187, 15));
+//	ft_printf("%o\n%o\n%o\n%o\n%o\n%o\n", 1, 5, 8, 9, 15, 90);
+//	printf("%o\n%o\n%o\n%o\n%o\n%o\n", 1, 5, 8, 9, 15, 90);
+//	ft_printf("%d\n", ft_printf("my ptf: %d\n%p\n%x\n%o\n", -2147483648, &x, -187, 15));
+//	printf("%d\n", printf("real ptf: %d\n%p\n%x\n%o\n", -2147483648, &x, -187, 15));
+//	printf("%%%c%%%s%%%d%%%i%%%u%%%x%%%X%%%% %%%c%%%s%%%d%%%i%%%u%%%x%%%X%%%% %%%c%%%s%%%d%%%i%%%u%%%x%%%X%%%% %c%%", 'A', "42", 42, 42 ,42 , 42, 42, 'B', "-42", -42, -42 ,-42 ,-42, 42, 'C', "0", 0, 0 ,0 ,0, 42, 0);
+	ft_printf("%%%c%%%s%%%d%%%i%%%u%%%x%%%X%%%% %%%c%%%s%%%d%%%i%%%u%%%x%%%X%%%% %%%c%%%s%%%d%%%i%%%u%%%x%%%X%%%% %c%%", 'A', "42", 42, 42 ,42 , 42, 42, 'B', "-42", -42, -42 ,-42 ,-42, 42, 'C', "0", 0, 0 ,0 ,0, 42, 0);
 	return (0);
 }
